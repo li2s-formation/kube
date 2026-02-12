2317eac490ebdc5aded7301da2f944d8eb238e56
samir-lakhdari <samir-lakhdari@users.noreply.github.com>
2026-02-12 09:55:39 +0000

    deploy: 7c5aaa94ea82a6f83e92421689e2ad4c0ff6abf4

---
+++ assets/slidev/default-DIaQJlu4.js
@@ -0,0 +1,1 @@
+import{o,e as t,i as s}from"../modules/vue-BkUzi_-h.js";import{_ as r}from"../index-BUbUA761.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
